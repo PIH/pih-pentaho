138a7d8d9a05d6e04d1feaebf2ac529a67d92c75
mseaton <mseaton@pih.org>
2016-11-02 11:39:33 -0400

    Add initial job to load star schema with dimensions

--- sql/dim_time.sql
+++ sql/dim_time.sql
@@ -1,6 +1,5 @@
 -- Time dimension
--- TODO: Loading in this way is inefficient, since there are only 24 x 60 possible values in this table
--- TODO: We should really just iterate and load them in
+-- TODO: We should represent "unknown" as an entry in this table somehow, to avoid null values in the fact table
 
 DROP TABLE IF EXISTS dim_time;
 
@@ -10,22 +9,12 @@ create table dim_time (
   minute INT
 );
 
-DELIMITER //
-CREATE PROCEDURE insert_time()
-  BEGIN
-    DECLARE hr int DEFAULT 0;
-    DECLARE min int DEFAULT 0;
-    WHILE hr < 24 DO
-      WHILE min < 60 DO
-        INSERT INTO dim_time (hour, minute) VALUES (hr, min);
-        SET min = min + 1;
-      END WHILE;
-      SET min = 0;
-      SET hr = hr + 1;
-    END WHILE;
-  END
-//
-
-CALL insert_time();
-
-DROP PROCEDURE insert_time;
+insert into dim_time (hour, minute)
+  SELECT DISTINCT hour(d.selected_date), minute(d.selected_date) FROM
+    (SELECT encounter_datetime as selected_date FROM omrs_encounter
+     UNION
+     SELECT obs_datetime FROM omrs_obs
+     UNION
+     SELECT value_datetime FROM omrs_obs
+    ) d
+;
