5491d9769f544d8c2a9f61f66793dc9122cf8a9f
mseaton <mseaton@pih.org>
2016-11-03 19:24:59 -0400

    Add gender to fact table directly

---
+++ sql/dim_gender.sql
@@ -0,0 +1,15 @@
+-- Create gender dimension
+
+DROP TABLE IF EXISTS dim_gender;
+
+create table dim_gender (
+  gender_id INT AUTO_INCREMENT PRIMARY KEY,
+  code char(1),
+  name_en varchar(15)
+);
+
+insert into dim_gender (code, name_en) values ('M', 'Male');
+insert into dim_gender (code, name_en) values ('F', 'Female');
+insert into dim_gender (code, name_en) values ('U', 'Unknown');
+
+ALTER TABLE dim_gender ADD INDEX dim_gender_code_idx (code);
--- sql/fact_patient_event.sql
+++ sql/fact_patient_event.sql
@@ -4,6 +4,7 @@ DROP TABLE IF EXISTS fact_patient_event;
 
 CREATE TABLE fact_patient_event (
   patient_id INT,
+  gender_id INT,
   age_id INT,
   date_id INT,
   time_id INT,
