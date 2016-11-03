066d7f044f7cc5d53b5f73bd1b087a8c24b8abf7
mseaton <mseaton@pih.org>
2016-11-02 21:55:57 -0400

    Modify age columns in tables to include both age in years and in months for key ages

--- sql/dim_age.sql
+++ sql/dim_age.sql
@@ -5,31 +5,27 @@ DROP TABLE IF EXISTS dim_age;
 
 create table dim_age (
   age_id INT AUTO_INCREMENT PRIMARY KEY,
-  birthdate DATE,
-  age_date DATE,
   full_years INT,
   full_months INT
 );
 
-insert into dim_age (birthdate, age_date, full_years, full_months)
-  select distinct d.birthdate, d.age_date, TIMESTAMPDIFF(YEAR, d.birthdate, d.age_date), TIMESTAMPDIFF(MONTH, d.birthdate, d.age_date) from
-  (SELECT birthdate, death_date as age_date FROM dim_patient where death_date is not null
+insert into dim_age (full_years, full_months)
+  select distinct yrs, mths from
+  (SELECT age_years_at_death as yrs, age_months_at_death mths FROM omrs_patient where age_months_at_death is not null
    UNION
-   SELECT p.birthdate, e.enrollment_date FROM omrs_patient p inner join omrs_program_enrollment e on p.uuid = e.patient_uuid where p.birthdate is not null
+   SELECT age_years_at_enrollment, age_months_at_enrollment FROM omrs_program_enrollment where age_months_at_enrollment is not null
    UNION
-   SELECT p.birthdate, e.completion_date FROM omrs_patient p inner join omrs_program_enrollment e on p.uuid = e.patient_uuid where p.birthdate is not null
+   SELECT age_years_at_completion, age_months_at_completion FROM omrs_program_enrollment where age_months_at_enrollment is not null
    UNION
-   SELECT p.birthdate, e.start_date FROM omrs_patient p inner join omrs_program_state e on p.uuid = e.patient_uuid where p.birthdate is not null
+   SELECT age_years_at_start, age_months_at_start FROM omrs_program_state where age_months_at_start is not null
    UNION
-   SELECT p.birthdate, e.end_date FROM omrs_patient p inner join omrs_program_state e on p.uuid = e.patient_uuid where p.birthdate is not null
+   SELECT age_years_at_end, age_months_at_end FROM omrs_program_state where age_months_at_end is not null
    UNION
-   SELECT p.birthdate, e.encounter_date FROM omrs_patient p inner join omrs_encounter e on p.uuid = e.patient_uuid where p.birthdate is not null
+   SELECT age_years_at_encounter, age_months_at_encounter FROM omrs_encounter where age_months_at_encounter is not null
    UNION
-   SELECT p.birthdate, e.obs_date FROM omrs_patient p inner join omrs_obs e on p.uuid = e.patient_uuid where p.birthdate is not null
-   UNION
-   SELECT p.birthdate, e.value_date FROM omrs_patient p inner join omrs_obs e on p.uuid = e.patient_uuid where p.birthdate is not null
+   SELECT age_years_at_obs, age_months_at_obs FROM omrs_obs where age_months_at_obs is not null
   ) d
 ;
 
-ALTER TABLE dim_age ADD INDEX dim_age_birthdate_idx (birthdate);
-ALTER TABLE dim_age ADD INDEX dim_age_agedate_idx (age_date);
+ALTER TABLE dim_age ADD INDEX dim_age_years_idx (full_years);
+ALTER TABLE dim_age ADD INDEX dim_age_months_idx (full_months);
