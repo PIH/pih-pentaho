4dc407ffdc5ceb4a781c7432a21536a8b21fd407
mseaton <mseaton@pih.org>
2017-03-07 22:06:32 -0500

    Modify omrs load process so that the schema drop and create is moved out of the loading process

---
+++ sql/omrs_schema_drop.sql
@@ -0,0 +1,13 @@
+
+-- Schema used to drop the omrs_* tables for flattened OpenMRS data
+
+drop table if exists omrs_patient;
+drop table if exists omrs_patient_identifier;
+drop table if exists omrs_relationship;
+drop table if exists omrs_encounter_provider;
+drop table if exists omrs_encounter;
+drop table if exists omrs_visit;
+drop table if exists omrs_obs_group;
+drop table if exists omrs_obs;
+drop table if exists omrs_program_enrollment;
+drop table if exists omrs_program_state;
