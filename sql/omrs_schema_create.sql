f8486b42d491912894377f34c59ab019358076a7
mgoodrich <mark.goodrich@live.com>
2017-03-14 11:40:15 -0400

    correct encounter provider merge add back in performance parameters for database in import into omrs-encounter

--- sql/omrs_schema_create.sql
+++ sql/omrs_schema_create.sql
@@ -62,6 +62,7 @@ CREATE TABLE omrs_encounter_provider (
   encounter_provider_id INT not null,
   uuid CHAR(38) not null,
   encounter_id INT not null,
+  encounter_uuid CHAR(38) not null,
   provider VARCHAR(255),
   provider_role VARCHAR(255)
 );
