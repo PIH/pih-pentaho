0902a9a114353b98a3b13e7e7d5a8f8cb91b37ff
mseaton <mseaton@pih.org>
2017-03-28 12:56:04 -0400

    Add Late NCD to Trace

--- sql/mw_schema_create.sql
+++ sql/mw_schema_create.sql
@@ -59,13 +59,13 @@ CREATE TABLE mw_pre_art_visits (
 );
 
 CREATE TABLE mw_pre_art_register (
-  enrollment_id             INT NOT NULL,
-  patient_id                INT NOT NULL,
-  location                  VARCHAR(255),
-  hcc_number                VARCHAR(50),
-  start_date                DATE,
-  end_date                  DATE,
-  outcome                   VARCHAR(100)
+  enrollment_id  INT NOT NULL,
+  patient_id     INT NOT NULL,
+  location       VARCHAR(255),
+  pre_art_number VARCHAR(50),
+  start_date     DATE,
+  end_date       DATE,
+  outcome        VARCHAR(100)
 );
 
 CREATE TABLE mw_art_visits (
