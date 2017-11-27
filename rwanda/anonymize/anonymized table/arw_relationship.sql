3251b154b9cbd90c0d061d72c77e1efab4f116bc
mseaton <mseaton@pih.org>
2017-11-27 18:13:19 -0500

    Attempt at reducing anonymize job to what is actually used.

--- rwanda/anonymize/anonymized table/arw_relationship.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE TABLE arw_relationship (
-  research_patient_id VARCHAR(32),
-  patient_role VARCHAR(50) not null,
-  related_person_role VARCHAR(50) not null,
-  start_date DATE,
-  end_date DATE,
-  date_created DATE
-);
