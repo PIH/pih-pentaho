a89634062b66df0320be4005b64085bdd7225a91
mseaton <mseaton@pih.org>
2017-11-28 10:51:23 -0500

    Add rw_ tables to anonymous schema.  Update schema with correct data types for non-anon schema.

--- rwanda/schema/table/rw_order.sql
+++ rwanda/schema/table/rw_order.sql
@@ -33,17 +33,3 @@ CREATE TABLE rw_order (
 );
 
 alter table rw_order add index rw_order_id(order_id);
-
-CREATE TABLE rw_order_group (
-  order_group_id varchar(32) not null,
-  uuid VARCHAR(255),
-  group_type VARCHAR(255),
-  cycle_number INT,
-  date_created date,
-  order_set_uuid VARCHAR(255),
-  order_set_name VARCHAR(255),
-  order_set_description VARCHAR(1023),
-  indication VARCHAR(255)
-);
-
-alter table rw_order_group add index rw_order_group_order_group_id(order_group_id);
