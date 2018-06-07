8b30b91b68f3e7fcdafed74700a55185acbcf675
mseaton <mseaton@pih.org>
2018-06-07 16:10:31 -0400

    Attempt to fix error "The tablename is not defined (empty)"

--- sql/azure/openmrs_warehouse_schema_create.sql
+++ sql/azure/openmrs_warehouse_schema_create.sql
@@ -44,7 +44,7 @@ CREATE TABLE omrs_obs
 , value_coded VARCHAR(255)
 , value_date DATETIME
 , value_numeric FLOAT(53)
-, value_text NVARCHAR(65535)
+, value_text VARCHAR(MAX)
 , comments VARCHAR(255)
 , obs_group_id INT
 , date_created DATETIME
