8bbbd1683f90bfe7dd75c97b8b5446a9cf2fa70c
Branford T. Gbieor <gbieorbranford@gmail.com>
2020-04-10 15:23:58 +0000

    customizations for liberia (#5)

---
+++ liberia/sql/omrs_schema_drop.sql
@@ -0,0 +1,16 @@
+
+-- Schema used to drop the omrs_* tables for flattened OpenMRS data
+
+drop table if exists omrs_patient;
+drop table if exists omrs_patient_identifier;
+drop table if exists omrs_relationship;
+drop table if exists omrs_encounter_provider;
+drop table if exists omrs_encounter;
+drop table if exists omrs_visit;
+drop table if exists omrs_obs_group;
+drop table if exists omrs_obs;
+drop table if exists omrs_program_enrollment;
+drop table if exists omrs_program_state;
+drop table if exists mh_encounter;
+drop table if exists omrs_program_statistics;
+drop table if exists omrs_rx_prescriptions;
